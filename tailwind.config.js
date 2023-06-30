58a9cf0afc51bee59987f3e1c009742fa11e7dc3
vidm27 <davidmoran464@gmail.com>
2023-06-30 14:54:35 -0500

    config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import { url } from 'inspector';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-desktop': url('src/assets/img/pattern-bg-desktop.png'),
+        'hero-mobile': url('src/assets/img/pattern-bg-mobile.png')
+      }
+    },
+  },
+  plugins: [],
+}
